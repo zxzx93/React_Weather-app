f399e3b297a5abf9c890d6ace843dc01091b102d
송연수 <song-yeonsu@song-yeonsuui-MacBookPro.local>
2019-05-23 12:23:55 +0900

    2019.05.23 변경

--- App.js
+++ App.js
@@ -2,46 +2,49 @@ import React, { Component } from "react";
 import {StyleSheet,Text,View,Image,ActivityIndicator,StatusBar} from "react-native";
 import Weather from "./Weather";
 
-const API_KEY = "6a02793735b1a0345302e08138de80f6" ;
+     //const API_KEY = "6a02793735b1a0345302e08138de80f6";
 
 export default class App extends Component {
   state = {
-    isLoaded: false,
-    error : null
+    isLoaded: true,
+    error: null
   };
 
   componentDidMount() {
     navigator.geolocation.getCurrentPosition(
-        position => {
-          this._getWeather(position.coords.latitude , position.coords.longitude )
+      position => {
+        this.setState({
+        error : "somthing went wrong"
+        });
       },
       error => {
         this.setState({
           error: error
         });
-  });
-}
+      }
+    );
+  }
 
-
-  _getWeather = (lat,lon) => {
-    fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&APPID=${API_KEY}`)
-    .then(response => response.json())
-      .then(json => {console.log(json)
+/*   _getWeather = (lat, lon) => {
+    fetch(
+      `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&APPID=${API_KEY}`
+    )
+      .then(response => response.json())
+      .then(json => {
+        console.log(json);
       });
-  };
+  }; */
 
   render() {
-    const { isLoaded ,error } = this.state;
+    const { isLoaded, error } = this.state;
 
     return (
       <View style={styles.container}>
         <StatusBar hidden={true} />
-        {isLoaded
-          ? <Weather />
+        {isLoaded ? (<Weather />)
           : <View style={styles.loading}>
-              <Text style={styles.loadingText}>
-                Getting the fucking weather
-              </Text>
+              <Text style={styles.loadingText}>Getting the fucking weather</Text>
+              { error ? <Text style={styles.errorText}>{error}</Text> : null }
             </View>}
       </View>
     );
@@ -53,6 +56,11 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#fff"
   },
+  errorText :{
+    color: "red",
+    backgroundColor: "transparent",
+    marginBottom : 40
+  },
   loading: {
     flex: 1,
     backgroundColor: "#FDF6AA",
@@ -61,7 +69,7 @@ const styles = StyleSheet.create({
   },
   loadingText: {
     fontSize: 38,
-    marginBottom: 24 
+    marginBottom: 24
   }
 });
 
